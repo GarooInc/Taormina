5788436374ffb941c81adde2038b24361aa6260b
Jimena Hernández <her21199@uvg.edu.gt>
2024-12-05 09:13:37 -0600

    Update layout.jsx

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -7,7 +7,7 @@ export const metadata = {
     title: 'Taormina',
     description: 'Located at San José, Costa Rica, Taormina Hotel & Casino is positioned at a historical neighborhood called Barrio Amón, which includes a set of coffee mansions built at the end of the 19th century and beginning of the 20th. Our hotel offers guests exclusive and high-quality services that allows anyone to discover the exotic side of San José.The modern and refined furniture, lovely common areas, stylish rooms and small details blend together to create a space where guests will have a pleasant stay. ',
     url: '',
-    image: '/assets/images/Favicon.png'
+    image: '/assets/images/logo_v1.png'
 }
 
 const RootLayout = ({children}) => {
@@ -15,7 +15,7 @@ const RootLayout = ({children}) => {
     <html lang="es">
         <head>
             <title>{metadata.title}</title>
-            <link rel="icon" type="image/png+xml" href="/assets/images/Favicon.png" />
+            <link rel="icon" type="image/png+xml" href={metadata.image} />
             <meta name="description" content={metadata.description} />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta property="og:title" content={metadata.title} />
